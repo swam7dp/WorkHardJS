92db4db175bc0ee8480914859b87fc7b2c5d5eca
Ievgenyi <swam7dp@gmail.com>
2019-05-15 11:22:16 +0300

    Первое задание v1

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let money = +prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-var x = prompt("Введите обязательную статью расходов в этом месяце");
-var y = +prompt("Во сколько обойдется");
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
 let appData = {
-    money: money,
+    budget: money,
     data: time,
-    expenses: {
-        [x]: y },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
@@ -14,5 +12,51 @@ let appData = {
 
 
 
-alert("Бюджет на один день: " + appData.money / 30);
+;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется");
+    if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
+        a != "" & b != "" && a.length < 50) {
+        console.log("done")
+        appData.expenses[a] = b;
+    } else {
+        var size = Object.keys(appData.expenses).length;
+        while (size <= 2) {
+            let a = prompt("Введите обязательную статью расходов в этом месяце"),
+                b = prompt("Во сколько обойдется");
+            if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
+                a != "" & b != "" && a.length < 50) {
+                console.log("done")
+                appData.expenses[a] = b;
+                var size = Object.keys(appData.expenses).length;
+            }
+
+        }
+
+    };
+
+    appData.moneyPerDay = appData.budget / 30;
+
+}
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Вы бомж")
+} else {
+    if (appData.moneyPerDay < 2000 && appData.moneyPerDay > 100) {
+        console.log("Средний уровень");
+    } else {
+        if (appData.moneyPerDay > 2000) {
+            console.log("Высокий уровень достатка");
+        } else {
+            console.log("Не верное значение бюджета");
+        }
+    }
+}
 console.log(appData);
+
+
+
+var size = Object.keys(appData.expenses).length;
+console.log(size);
